e1766285f7bad8784e5cee2afb89e988da9ddc8f
Jannis Blossey <jannis@blossey.eu>
2021-11-06 14:56:04 +0100

    add salt and watermark calc

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import * as argon2 from 'argon2';
 import Buffer from 'buffer';
 
-export async function calculateWatermark(creatorId: Buffer, license: Buffer): Promise<Buffer> {
+export async function calculateSalt(creatorId: Buffer, license: Buffer): Promise<Buffer> {
   const hashOptions: argon2.Options & { raw: true } = {
-    hashLength: 64,
+    hashLength: 128,
     type: argon2.argon2id,
     salt: creatorId,
     saltLength: creatorId.length,
@@ -12,55 +12,44 @@ export async function calculateWatermark(creatorId: Buffer, license: Buffer): Pr
   return argon2.hash(license, hashOptions);
 }
 
-export async function calculateContractId(
+export async function calculateWatermark(
   blob: Buffer,
-  watermark: Buffer
+  salt: Buffer
 ): Promise<Buffer>;
 
-export async function calculateContractId(
+export async function calculateWatermark(
   blob: Buffer,
   creatorId: Buffer,
   license: Buffer
 ): Promise<Buffer>;
 
-export async function calculateContractId(...args: any[]): Promise<Buffer> {
-  let contractId: Promise<Buffer>;
-  switch (args.length) {
-    case 2:
-      contractId = (
-        blob: Buffer = args[0],
-        watermark: Buffer = args[1],
-      ): Promise<Buffer> => {
-        const hashOptions: argon2.Options & { raw: true; } = {
-          hashLength: 128,
-          type: argon2.argon2id,
-          salt: watermark,
-          saltLength: watermark.length,
-          raw: true,
-        };
-        return argon2.hash(blob, hashOptions);
-      };
-      break;
-    case 3:
-      contractId = (async (
-        blob: Buffer = args[0],
-        creatorId: Buffer = args[1],
-        license: Buffer = args[2],
-      ) => {
-        const watermark = await calculateWatermark(creatorId, license);
-        const hashOptions: argon2.Options & { raw: true } = {
-          hashLength: 128,
-          type: argon2.argon2id,
-          salt: watermark,
-          saltLength: watermark.length,
-          raw: true,
-        };
-        return argon2.hash(blob, hashOptions);
-      });
-      break;
-    default:
-      Error('Wrong number of arguments. Expect two or three.');
-      break;
+export async function calculateWatermark(...args: any[]): Promise<Buffer> {
+  if (args.length === 2) {
+    const blob: Buffer = args[0];
+    const salt: Buffer = args[1];
+    const hashOptions: argon2.Options & { raw: true; } = {
+      hashLength: 128,
+      type: argon2.argon2id,
+      salt,
+      saltLength: salt.length,
+      raw: true,
+    };
+    return argon2.hash(blob, hashOptions);
   }
-  return contractId;
+  if (args.length === 3) {
+    const blob: Buffer = args[0];
+    const creatorId: Buffer = args[1];
+    const license: Buffer = args[2];
+    const salt = await calculateSalt(creatorId, license);
+    const hashOptions: argon2.Options & { raw: true } = {
+      hashLength: 128,
+      type: argon2.argon2id,
+      salt,
+      saltLength: salt.length,
+      raw: true,
+    };
+    return argon2.hash(blob, hashOptions);
+  }
+  Error('Wrong number of arguments. Expect two or three.');
+  return new Promise(() => {});
 }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,13 +1,40 @@
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
 import chai from 'chai';
 import { randomBytes } from 'crypto';
-import { calculateWatermark } from '../src/index';
+import { calculateSalt, calculateWatermark } from '../src/index';
 import 'mocha';
 
-describe('calculate watermark', () => {
-  it('should calculate the same watermark for the same inputs twice', async () => {
+describe('calculate salt', () => {
+  it('should calculate the same salt for the same inputs twice', async () => {
     const creatorIdBuffer = Buffer.from(randomBytes(128));
     const licenseBuffer = Buffer.from('GPL 3.0');
+    chai.expect((await calculateSalt(creatorIdBuffer, licenseBuffer)).toString('hex'))
+      .to.be.equal((await calculateSalt(creatorIdBuffer, licenseBuffer)).toString('hex'));
+  });
+});
+
+describe('calculate watermark', function () {
+  this.timeout(15000);
+  it('should calculate the same watermark for the same inputs twice', async () => {
+    const creatorIdBuffer = Buffer.from(randomBytes(128));
+    const licenseBuffer = Buffer.from(randomBytes(256));
     chai.expect((await calculateWatermark(creatorIdBuffer, licenseBuffer)).toString('hex'))
       .to.be.equal((await calculateWatermark(creatorIdBuffer, licenseBuffer)).toString('hex'));
+  });
+  it('should be able to take two (large) inputs', async () => {
+    const saltBuffer = Buffer.from(randomBytes(128));
+    const blobBuffer = Buffer.from(randomBytes(2 * (10 ** 9))); // 2GB -> can take up to 2**32-1
+    const first = await calculateWatermark(blobBuffer, saltBuffer);
+    const second = await calculateWatermark(blobBuffer, saltBuffer);
+    chai.expect(first.toString('hex')).to.be.equal(second.toString('hex'));
+  });
+  it('should be able to take three (large) inputs', async () => {
+    const creatorIdBuffer = Buffer.from(randomBytes(128));
+    const licenseBuffer = Buffer.from(randomBytes(256));
+    const blobBuffer = await Buffer.from(randomBytes(2 * (10 ** 9)));
+    const first = await calculateWatermark(blobBuffer, creatorIdBuffer, licenseBuffer);
+    const second = await calculateWatermark(blobBuffer, creatorIdBuffer, licenseBuffer);
+    chai.expect(first.toString('hex')).to.be.equal(second.toString('hex'));
   });
 });
