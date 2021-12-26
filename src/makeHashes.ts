9c16062b80f8546983d59f3772a866f92d7d43c7
Jannis Blossey <jannis@blossey.eu>
2021-12-26 23:48:26 +0100

    doc(library): add typedocs to all that were still missing some

--- src/makeHashes.ts
+++ src/makeHashes.ts
@@ -1,7 +1,12 @@
 import * as argon2 from 'argon2';
 import Buffer from 'buffer';
 
-// eslint-disable-next-line max-len
+/**
+ * Calculate 128 byte hash of creatorId as salt and license as main input
+ * @param creatorId Buffer that holds a creatorId
+ * @param license Buffer that holds the license
+ * @returns Buffer Promise that resolves with a 128 byte long hash
+ */
 export async function calculateCreatorIdAndLicenseHash(creatorId: Buffer, license: Buffer): Promise<Buffer> {
   const hashOptions: argon2.Options & { raw: true } = {
     hashLength: 128,
@@ -13,6 +18,13 @@ export async function calculateCreatorIdAndLicenseHash(creatorId: Buffer, licens
   return argon2.hash(license, hashOptions);
 }
 
+/**
+ * Private convenience function that calculates argon2 hashes of varying length from a salt and main input
+ * @param hashLength the length in bytes that the returned hash will have
+ * @param salt the hash's salt
+ * @param input the main input
+ * @returns argon2 hash of hashLength length
+ */
 async function calculateHash(hashLength: number, salt: Buffer, input: Buffer): Promise<Buffer> {
   const hashOptions: argon2.Options & { raw: true; } = {
     hashLength,
@@ -24,17 +36,32 @@ async function calculateHash(hashLength: number, salt: Buffer, input: Buffer): P
   return argon2.hash(input, hashOptions);
 }
 
+/**
+ * Calculates a watermark that can be used to register digital goods on properli's LawCompliersAgreement smart contract
+ * @param blob Buffer that has size <2^(32-1)
+ * @param salt Buffer that has size <2^(32-1)
+ */
 export async function calculateWatermark(
   blob: Buffer,
   salt: Buffer
 ): Promise<Buffer>;
 
+/**
+ * Calculates a watermark that can be used to register digital goods on properli's LawCompliersAgreement smart contract
+ * @param blob Buffer that has size <2^(32-1)
+ * @param creatorId a unique identifier for the issuing creator of the digital good
+ * @param license the license under which the digital good should be published
+ */
 export async function calculateWatermark(
   blob: Buffer,
   creatorId: Buffer,
   license: Buffer
 ): Promise<Buffer>;
 
+/**
+ * Calculates a watermark that can be used to register digital goods on properli's LawCompliersAgreement smart contract.
+ * @returns Promise Buffer that resolves with a hash that can be used watermark a digital good and register it on properli's LawCompliersAgreement smart contract.
+ */
 export async function calculateWatermark(...args: any[]): Promise<Buffer> {
   if (args.length === 2) {
     const blob: Buffer = args[0];
@@ -51,6 +78,9 @@ export async function calculateWatermark(...args: any[]): Promise<Buffer> {
   return new Promise(() => {});
 }
 
+/**
+ * export object that is used for testing only
+ */
 export const testables = {
   calculateCreatorIdAndLicenseHash,
   calculateHash,
--- src/textWatermarking.ts
+++ src/textWatermarking.ts
@@ -12,6 +12,9 @@ import * as _ from 'lodash';
  */
 const watermarkBinSeparator: string[] = _.fill(Array(41), '1');
 
+/**
+ * 'Normal' encodings of confusable characters when using utf8
+ */
 const confusableCharacters = [
   '\u002d', // -
   '\u003b', // ;
@@ -31,6 +34,9 @@ const confusableCharacters = [
   '\u0078', // x
 ];
 
+/**
+ * 'Alternative' encoding of confusable characters
+ */
 const reverseConfusableCharacters: string[] = [
   '\u2010', // -
   '\u037e', // ;
@@ -50,6 +56,9 @@ const reverseConfusableCharacters: string[] = [
   '\u2179', // x
 ];
 
+/**
+ * All possible encodings of whitespace characters
+ */
 const confusableWhitespaces = [
   '\u0020', // space
   '\u2000', // en quad
@@ -61,8 +70,14 @@ const confusableWhitespaces = [
   '\u205f', // medium mathematical space
 ];
 
+/**
+ * The complete set of confusables: characters + whitespaces
+ */
 const confusables = confusableCharacters.concat(confusableWhitespaces);
 
+/**
+ * Iterable collection of 'normal' characters and their 'alternative' confusables.
+ */
 const confusableCharacterMapping: { [key: string]: string; } = {
   '\u002d': '\u2010', // -
   '\u003b': '\u037e', // ;
@@ -82,6 +97,9 @@ const confusableCharacterMapping: { [key: string]: string; } = {
   '\u0078': '\u2179', // x
 };
 
+/**
+ * Iterable collection of the bit representation that is mapped to each confusable whitespace representation
+ */
 const confusableWhiteSpaceMapping: { [key: string]: string; } = {
   '000': '\u0020', // space
   '001': '\u2000', // en quad
@@ -93,6 +111,9 @@ const confusableWhiteSpaceMapping: { [key: string]: string; } = {
   '111': '\u205f', // medium mathematical space
 };
 
+/**
+ * Iterable collection of each whitespace representation and its corresponding bit representation in the watermark
+ */
 const reverseConfusableWhiteSpaceMapping: { [key: string]: string; } = {
   '\u0020': '000', // space
   '\u2000': '001', // en quad
@@ -105,7 +126,10 @@ const reverseConfusableWhiteSpaceMapping: { [key: string]: string; } = {
 };
 
 /**
- * http://stackoverflow.com/questions/45053624/ddg#45054052
+ * Transform a string representing a hex number into a string representing its binary equivalent.
+ * Taken from here http://stackoverflow.com/questions/45053624/ddg#45054052
+ * @param hex hexadecimal number as string
+ * @returns binary representation of hex as string
  */
 function hexToBin(hex: string): string {
   return (parseInt(hex, 16).toString(2)).padStart(8, '0');
@@ -115,8 +139,11 @@ function hexToBin(hex: string): string {
  * Modified version of Rizzo et al.'s fine grain watermarking.
  * This version clearly indicates the beginning of a repeatable watermark by starting every iteration with watermark.lengthInBits+1 number of '1's.
  * In the custom case of 40 Bit of embeddable information, the watermark is actually 81 Bits long.
- * @param textFilePath
- * @param watermark
+ * More on unicode in javascript here: https://dmitripavlutin.com/what-every-javascript-developer-should-know-about-unicode/
+ * @param textFilePath path to utf8-encoded .txt file from which the encodable text should be taken
+ * @param watermark Buffer containing the hexadecimal encoded watermark that should be embedded into the text from the textFilePath file
+ * @param outputFilePath optional string indicating the name and destination of the textfile with embedded watermark. Defaults to '<textFilePath>-watermarked.txt'.
+ * @returns Void Promise that resolves upon the finish of the write procedure
  */
 export const embedWatermark = (textFilePath: string, watermark: Buffer, outputFilePath?: string) => new Promise<void>((resolve) => {
   const readStream = createReadStream(
@@ -135,9 +162,6 @@ export const embedWatermark = (textFilePath: string, watermark: Buffer, outputFi
   const watermarkBin = watermarkBinSeparator.concat(hexToBin(watermark.toString('hex')).split(''));
   let watermarkPointer = 0;
   let modifiedChunk;
-
-  // https://dmitripavlutin.com/what-every-javascript-developer-should-know-about-unicode/
-  // http://www.herongyang.com/Unicode/UTF-16-UTF-16LE-Encoding.html
 
   const exchangeConfusables = new Transform({
     transform(chunk: Buffer, encoding, callback) {
@@ -169,25 +193,42 @@ export const embedWatermark = (textFilePath: string, watermark: Buffer, outputFi
   writeStream.on('finish', resolve);
 });
 
+/**
+ * What the declaration says: searches for a marker and extracts one complete watermark in correct order.
+ * @param watermarkArray string array that can contain an arbitrary amount of watermarks and markers
+ * @returns string array that contains a single watermark
+ */
 function rotateWatermarkToStartAndPurgeMarker(watermarkArray: string[]): string[] {
   const watermarkJoinedArray = watermarkArray.join('').substring(0, 81).split('');
   if (watermarkJoinedArray.length !== 81) throw new Error(`Watermark length was ${watermarkJoinedArray.length} but should have been 81.`);
   let markerStart: number = 0;
   let markerEnd: number = markerStart + watermarkBinSeparator.length;
   let notFullySearched = true;
-  // Locate Marker
   while (notFullySearched) {
-    // re-establish bit-sequence order
-    if (markerStart === 0 && watermarkJoinedArray.slice(markerStart, markerEnd).every((bit) => bit === '1')) { // marker is at the beginning of bit sequence, e.g. ----||||
+    if (// marker is at the beginning of bit sequence, e.g. ----||||
+      markerStart === 0
+      && watermarkJoinedArray.slice(markerStart, markerEnd).every((bit) => bit === '1')
+    ) {
       return watermarkJoinedArray.slice(markerEnd);
     }
-    if (markerEnd < markerStart && watermarkJoinedArray.slice(markerStart).every((bit) => bit === '1') && watermarkJoinedArray.slice(0, markerEnd).every((bit) => bit === '1')) { // marker wraps around and frames the bit sequence, e.g. --||||--
+    if (// marker wraps around and frames the bit sequence, e.g. --||||--
+      markerEnd < markerStart
+      && watermarkJoinedArray.slice(markerStart).every((bit) => bit === '1')
+      && watermarkJoinedArray.slice(0, markerEnd).every((bit) => bit === '1')
+    ) {
       return watermarkJoinedArray.slice(markerEnd, markerStart);
     }
-    if (markerStart !== 0 && markerStart < markerEnd && watermarkJoinedArray.slice(markerStart, markerEnd).every((bit) => bit === '1')) { // marker is framed by bit sequence, e.g. ||----||
+    if ( // marker is framed by bit sequence, e.g. ||----||
+      markerStart !== 0
+      && markerStart < markerEnd
+      && watermarkJoinedArray.slice(markerStart, markerEnd).every((bit) => bit === '1')
+    ) {
       return watermarkJoinedArray.slice(markerEnd).concat(watermarkJoinedArray.slice(0, markerStart));
     }
-    if (markerEnd === watermarkJoinedArray.length - 1 && watermarkJoinedArray.slice(markerStart, markerEnd).every((bit) => bit === '1')) { // marker is at the end of bit sequence, e.g. ||||----
+    if ( // marker is at the end of bit sequence, e.g. ||||----
+      markerEnd === watermarkJoinedArray.length - 1
+      && watermarkJoinedArray.slice(markerStart, markerEnd).every((bit) => bit === '1')
+    ) {
       return watermarkJoinedArray.slice(0, markerStart);
     }
     markerStart = (markerStart + 1) % watermarkJoinedArray.length;
@@ -198,8 +239,9 @@ function rotateWatermarkToStartAndPurgeMarker(watermarkArray: string[]): string[
 }
 
 /**
- *
- * @param textFilePath
+ * Extracts a watermark as hexadecimal string from a file
+ * @param textFilePath file path as string to file from which the watermark should be extracted
+ * @returns String Promise that resolves once a watermark could be successfully read from a file or rejects with an error otherwise.
  */
 export const extractWatermark = (textFilePath: string) => new Promise<string>((resolve, reject) => {
   const readStream = createReadStream(
@@ -237,6 +279,9 @@ export const extractWatermark = (textFilePath: string) => new Promise<string>((r
   });
 });
 
+/**
+ * all functions from this file exported for testing purposes
+ */
 export const testables = {
   hexToBin,
   embedWatermark,
